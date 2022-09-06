4dbbf5336e95023916029061fd11986279993e59
grgojelavic <grgo.jelavic@aspira.hr>
2022-09-06 23:17:44 +0200

    header and search components

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Search from './screens/Search';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Search />
+      <StatusBar style='auto' />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#00aaff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
